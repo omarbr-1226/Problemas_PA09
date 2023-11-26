043d9fe0a2389cd0dd836a1b8704a2504fa8c014
Omar Bautista <bautistaramirezomar0@gmail.com>
2023-11-26 15:26:32 -0600

    PROBLEMA_2 resuelto

--- PROBLEMA_2.java
+++ PROBLEMA_2.java
@@ -7,6 +7,7 @@ IVA; si el promedio obtenido es menor que 9 deberá pagar la colegiatura complet
 el 10% de IVA. Obtener cuanto debe pagar un alumno.
  */
 package Problemas_PA09;
+import java.util.Scanner;
 
 /**
  *
@@ -14,8 +15,62 @@ package Problemas_PA09;
  * Elaborado:
  */
 public class PROBLEMA_2 {
+    //Modulo principal
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int T,costo,prom;
+        char resp;
+
+        do{
+            System.out.println("Ingresa la cantidad de materias que cursan");
+            T = sc.nextInt();
+
+            System.out.println("Ingresa el costo de las materias");
+            costo=sc.nextInt();
+
+            System.out.println("Ingresa el promedio final");
+            prom= sc.nextInt();
+
+            float cole= Colegiatura(T,costo);
+            System.out.println("La colegiatura a pagar es: ");
+            System.out.println(cole);
+            System.out.println(" ");
+
+            if (prom>=9){
+                System.out.println("Su promedio fue mayor a nueve por lo que se le hare un descuento del 30%");
+                System.out.println("El total a pagar es: ");
+                System.out.println(Descuento(prom,cole));
+            }else{
+                System.out.println("Su promedio fue menor a nueve por lo que se le cobrara un 10% más");
+                System.out.println("El total a pagar es: ");
+                System.out.println(Comision(prom,cole));
+            }
+
+            System.out.println("Desea agregar otro alumno? (s/n)");
+            resp = sc.next().charAt(0);
+
+        }while(resp=='s' || resp=='S');
     }
-    
+    //Modulo uno
+    public static float Colegiatura(float a,float b){
+        float colegiatura;
+        colegiatura=a*b;
+        return colegiatura;
+    }
+    //Modulo dos
+    public static double Descuento(float cali,float coleg){
+        double descuento=0;
+        if(cali>=9){
+            descuento=coleg-(coleg*0.30);
+        } 
+        return descuento;
+    }
+    //Modulo tres
+    public static double Comision(double cali,double coleg ){
+        double comision=0;
+        if (cali<9){
+            comision=coleg+(coleg*0.10);
+        }
+        return comision;
+    }
 }
