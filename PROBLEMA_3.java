c8c6a17e550f46233a7eab7931badd0ee5535f8f
Omar Bautista <bautistaramirezomar0@gmail.com>
2023-11-26 02:04:03 -0600

    PROBLEMA_3 completo

--- PROBLEMA_3.java
+++ PROBLEMA_3.java
@@ -5,6 +5,8 @@ numero primo o no.
  */
 package Problemas_PA09;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Omar Bautista
@@ -12,7 +14,19 @@ package Problemas_PA09;
  */
 public class PROBLEMA_3 {
     public static void main(String[] args) {
-      
+        Scanner sc= new Scanner(System.in);
+        int num;
+        System.out.println("Ingresa un numero");
+        num = sc.nextInt();
+        Numero(num);
     }
-    
+    public static int Numero(int a){
+        int primo;
+        if (a %2==0){
+            System.out.println("es primo");
+        }else{
+            System.out.println("no es primo");
+        }
+    return 0;
+    }
 }
