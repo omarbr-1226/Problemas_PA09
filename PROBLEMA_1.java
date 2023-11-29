1f10dd3e331f4b08663f0a6e0bbe729b6edf6707
Omar Bautista <bautistaramirezomar0@gmail.com>
2023-11-29 13:28:34 -0600

    EJERCICIO_1 medio resuelto

--- PROBLEMA_1.java
+++ PROBLEMA_1.java
@@ -12,7 +12,7 @@ impuesto.
 • Al final se deberá imprimir el total a pagar a los trabajadores y a los empleados.
  */
 package Problemas_PA09;
-
+import java.util.Scanner;
 /**
  *
  * @author Omar Bautista
@@ -20,7 +20,38 @@ package Problemas_PA09;
  */
 public class PROBLEMA_1 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int hrs[]=new int[102],sueldHr[]=new int[102],obrero=0,empleado=0;
+        String trabajadores[]=new String[102];
+        //Inicializar vectores
+        for (int i = 0; i < 102; i++) {
+            hrs[i]=0;
+            sueldHr[i]=0;
+        }
+        //Llena vectores
+        for (int i = 0; i < 102; i++) {
+            System.out.println("/////////////// Ingrese los datos para el trabajador numero "+(i+1)+" ///////////////");
+             System.out.println("Ingresa las horas trabajadas");
+            hrs[i]=sc.nextInt();
+            System.out.println("Ingresa el sueldo por hora");
+            sueldHr[i]=sc.nextInt();
+            System.out.println("Que tipo de trabajador eres? ");
+            trabajadores[i]=sc.next();
+            System.out.println("----------------------------------------------------------------------------------------------------------------------------");
+        }
+       
+    }
+    public  static int Gacman(int x,int y){
+        double impuesto=0;
+        for (int i = 0; i < 102; i++) {
+            if (x<100000) {
+                System.out.println("");
+                y=0;
+            }else{
+                impuesto=(x*0.10)*x;
+            }
+        }
+        return 0;
     }
     
 }
